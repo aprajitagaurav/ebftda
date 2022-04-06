7c01f24aac1b69ed47384703fb8db9de84657d5f
Sai Neeraj Kanuri <neerajkanuri@gmail.com>
2022-04-06 15:53:49 -0400

    init

---
+++ Sources/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Team EBFTDA on 06/04/22.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+ebftda: ./Sources/main.cpp
+	    g++ -std=c++11 ./Sources/main.cpp
+
+clean:
+		rm ./a.out
