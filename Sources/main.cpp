87ed2b9b18fc2f3fd5b884b37c630a3815696c69
SripranavSureshKumar <78806718+SripranavSureshKumar@users.noreply.github.com>
2022-04-09 19:12:39 -0400

    Parallel Data Reads

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -8,42 +8,74 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <string>
+#include <math.h>
+#include <mpi.h>
 #include <unistd.h>
 
 using namespace std;
 
+int numberOfProcessors;
+int processorId;
+int fileCount = 5;
+
+void readFiles(int processorId) {
+
+    string inputFileName = "./data/eth-tx-";
+
+    int filesPerProcessor = ceil(fileCount/(float) numberOfProcessors);
+    
+    vector<Transaction> transactions;
+    set<string> addresses;
+
+    for(int i = 0; i < filesPerProcessor; i++){
+        int index = processorId*filesPerProcessor + i + 1;
+        
+        if(index > fileCount)
+            break;
+        
+        string inputFile = inputFileName + to_string(index) + ".txt";
+        Reader reader;
+        reader.init(inputFile);
+
+        while (!reader.isEofReached()){
+            vector<string> txn = reader.getProcessValues();
+            string from = txn.at(txn.size() - 3);
+            string to = txn.at(txn.size() - 2);
+
+            reader.populateNextLine();
+
+            Transaction transaction(from, to);
+
+            transactions.push_back(transaction);
+
+            addresses.insert(transaction.getFrom());
+            addresses.insert(transaction.getTo());
+        }
+
+    }
+
+    // for (int i = 0; i < transactions.size(); i++){
+    //     std::cout << "Processor Id: " << processorId << " From: " << transactions.at(i).getFrom() << " To: " << transactions.at(i).getTo() << "\n";
+    // }
+
+    for (std::set<std::string>::iterator it=addresses.begin(); it!=addresses.end(); ++it)
+        std::cout << "Processor Id: " << processorId << " Address: " << *it << "\n";
+
+}
+
 int main(int argc, char** argv) {
     // TODO : change to read from command line / MPI ...
     //        support multiple files per process
-    string inputFile = "./data/eth-tx-0-999999.txt";
 
-    // Input file reading
-    Reader reader;
-    reader.init(inputFile);
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcessors);
+    MPI_Comm_rank(MPI_COMM_WORLD, &processorId);
 
-    vector<Transaction> transactions;
+    readFiles(processorId);
 
-    set<string> addresses;
-
-    while (!reader.isEofReached()){
-        vector<string> txn = reader.getProcessValues();
-        string from = txn.at(txn.size() - 3);
-        string to = txn.at(txn.size() - 2);
-
-        reader.populateNextLine();
-
-        Transaction transaction(from, to);
-
-        transactions.push_back(transaction);
-
-//        printf("%s %s\n", transaction.getFrom().c_str(), transaction.getTo().c_str());
-
-        addresses.insert(transaction.getFrom());
-        addresses.insert(transaction.getTo());
-    }
-
-//    for (std::set<std::string>::iterator it=addresses.begin(); it!=addresses.end(); ++it)
-//        std::cout << *it << "\n";
+    MPI_Finalize();
+    
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 ebftda: ./Sources/main.cpp ./Sources/Reader.cpp ./Sources/Transaction.cpp
-	    g++ -std=c++11 ./Sources/main.cpp ./Sources/Reader.cpp ./Sources/Transaction.cpp
-
+	    mpicxx -o output ./Sources/main.cpp ./Sources/Reader.cpp ./Sources/Transaction.cpp
+		mpirun ./output
 clean:
-		rm ./a.out
+		rm output
